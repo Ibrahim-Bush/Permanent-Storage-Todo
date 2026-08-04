27caaa852ce37444a500f147cd40a623f596fb5b
Ibrahim Mohamed Bush <ibrahimbush2006@gmail.com>
2026-08-04 14:11:54 +0300

    feat: define todo model and request DTOs

---
+++ model/todo_models.go
@@ -0,0 +1,31 @@
+package models
+
+import "time"
+
+type Todo struct {
+	ID          int    	   `json:"id" gorm:"primaryKey"`
+	Title       string	   `json:"title" gorm:"type:varchar(255);not null"`
+	Completed   bool  	   `json:"completed" gorm:"default:false;not null"`
+	Category    string	   `json:"category" gorm:"type:varchar(50);not null"`
+	Priority    string 	   `json:"priority" gorm:"type:varchar(10);not null"`
+	CompletedAt *time.Time `json:"completedAt" gorm:"type:datetime;default:null"`
+	DueDate		*time.Time `json:"dueDate" gorm:"type:datetime;default:null"`
+}
+
+type Create_todo_request struct{
+	Title		string 	 `json:"title"`
+	Completed   bool	 `json:"completed"`
+	Category	string	 `json:"category"`
+	Priority	string	 `json:"priority"`
+	DueDate		string	 `json:"dueDate"`
+}
+
+type Update_todo_request struct{
+	//we use pointers to differentiate default zero-valued from user input.
+	Title	  *string	`json:"title"`
+	Completed *bool		`json:"completed"`
+	Category  *string	`json:"category"`
+	Priority  *string	`json:"priority"`
+	DueDate	  *string	`json:"dueDate"`
+}
+
