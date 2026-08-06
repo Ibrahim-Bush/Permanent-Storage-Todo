41517d6c825dd69916a379cbe39afa6adbf886f6
Ibrahim Mohamed Bush <ibrahimbush2006@gmail.com>
2026-08-06 21:48:38 +0300

    feat: create request handler layer

---
+++ handler/todo_handlers.go
@@ -0,0 +1,218 @@
+package handler
+
+import (
+	"Permanent-Storage-Todo/model"
+	"Permanent-Storage-Todo/service"
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Todo_handler struct {
+	service service.Todo_service
+}
+
+func Init_handler(service service.Todo_service) *Todo_handler {
+	new_handler := Todo_handler{service: service}
+	return &new_handler
+}
+
+func (handler *Todo_handler) Create_todo_handler(c *gin.Context) {
+	//get the data from json body.
+	var data model.Create_todo_request
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid json"})
+		return
+	}
+	//create new todo with the input data.
+	todo_ptr, err := handler.service.Create_todo(data)
+	//check if something went wrong.
+	switch {
+	case err == nil:
+		c.JSON(200, *todo_ptr)
+	case errors.Is(err, service.ErrEmptyTitle):
+		c.JSON(400, gin.H{"error": "Title cannot be empty"})
+	case errors.Is(err, service.ErrEmptyCategory):
+		c.JSON(400, gin.H{"error": "Category cannot be empty"})
+	case errors.Is(err, service.ErrInvalidPriority):
+		c.JSON(400, gin.H{"error": "Priority must be one of (low, medium, high)"})
+	case errors.Is(err, service.ErrInvalidDueDate):
+		c.JSON(400, gin.H{"error": "Unsupported due date format"})
+	case errors.Is(err, service.ErrTimeInPast):
+		c.JSON(400, gin.H{"error": "Due date must be in the future"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Update_todo_by_id_handler(c *gin.Context) {
+	//first take the id param.
+	id := c.Param("id")
+	//then take the json body with struct.
+	var data model.Update_todo_request
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid json"})
+		return
+	}
+	//finally, update required item with update function.
+	todo_ptr, err := handler.service.Update_todo_by_id(id, data)
+	//check if something went wrong.
+	switch {
+	case err == nil:
+		c.JSON(200, *todo_ptr)
+	case errors.Is(err, service.ErrInvalidId):
+		c.JSON(400, gin.H{"error": "Invalid todo id"})
+	case errors.Is(err, service.ErrEmptyTitle):
+		c.JSON(400, gin.H{"error": "Title cannot be empty"})
+	case errors.Is(err, service.ErrEmptyCategory):
+		c.JSON(400, gin.H{"error": "Category cannot be empty"})
+	case errors.Is(err, service.ErrInvalidPriority):
+		c.JSON(400, gin.H{"error": "Priority must be one of (low, medium, high)"})
+	case errors.Is(err, service.ErrNotFound):
+		c.JSON(404, gin.H{"error": "Todo not found"})
+	case errors.Is(err, service.ErrInvalidDueDate):
+		c.JSON(400, gin.H{"error": "Unsupported due date format"})
+	case errors.Is(err, service.ErrTimeInPast):
+		c.JSON(400, gin.H{"error": "Due date must be in the future"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Update_todos_by_category_handler(c *gin.Context) {
+	//first take the category param.
+	category := c.Param("category")
+	//get the completed status from json body.
+	var data model.Update_todo_request
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid json"})
+		return
+	}
+	//check if completed field not entered.
+	if data.Completed == nil {
+		c.JSON(400, gin.H{"error": "Missing new completed status"})
+		return
+	}
+	//update todos of passed category.
+	new_status := *(data.Completed)
+	err = handler.service.Update_todos_by_category(category, new_status)
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, gin.H{"message": "Todos updated successfully"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Delete_todo_by_id_handler(c *gin.Context) {
+	//get the id param.
+	id := c.Param("id")
+	//delete todo of that ID.
+	err := handler.service.Delete_todo_by_id(id)
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, gin.H{"message": "Todo deleted successfully"})
+	case errors.Is(err, service.ErrInvalidId):
+		c.JSON(400, gin.H{"error": "Invalid todo id"})
+	case errors.Is(err, service.ErrNotFound):
+		c.JSON(404, gin.H{"error": "Todo not found"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Delete_all_todos_handler(c *gin.Context) {
+	//delete all todos in the system.
+	err := handler.service.Delete_all_todos()
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, gin.H{"message": "All todos deleted successfully"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Get_todo_by_id(c *gin.Context) {
+	//get id parameter.
+	id := c.Param("id")
+	//get the todo of that id.
+	todo_ptr, err := handler.service.Get_todo_by_id(id)
+	//check if something went wrong.
+	switch {
+	case err == nil:
+		c.JSON(200, *todo_ptr)
+	case errors.Is(err, service.ErrInvalidId):
+		c.JSON(400, gin.H{"error": "Invalid todo id"})
+	case errors.Is(err, service.ErrNotFound):
+		c.JSON(404, gin.H{"error": "Todo not found"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Get_todos_by_category_handler(c *gin.Context) {
+	//get category param
+	category := c.Param("category")
+	//get all todos of that category.
+	todos, err := handler.service.Get_todos_by_category(category)
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, todos)
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Get_todos_by_status(c *gin.Context) {
+	//get status param.
+	status := c.Param("status")
+	//get todos of that status.
+	todos, err := handler.service.Get_todos_by_status(status)
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, todos)
+	case errors.Is(err, service.ErrInvalidStatus):
+		c.JSON(400, gin.H{"error": "Invalid todo status"})
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Get_todos_by_title_handler(c *gin.Context) {
+	//get the query param.
+	title, exist := c.GetQuery("q")
+	//check if it exist or not.
+	if !exist {
+		c.JSON(400, gin.H{"error": "Missing query param for searching"})
+		return
+	}
+	//get todos that contain this query param.
+	todos, err := handler.service.Get_todos_by_title(title)
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, todos)
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
+
+func (handler *Todo_handler) Get_all_todos_handler(c *gin.Context) {
+	//Get all todos in system.
+	todos, err := handler.service.Get_all_todos()
+	//check the result of process.
+	switch {
+	case err == nil:
+		c.JSON(200, todos)
+	default:
+		c.JSON(500, gin.H{"error": "Server error"})
+	}
+}
